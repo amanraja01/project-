694113ef676660ade11d7dfee14322945f1b68a6
aman <aman21012002@gmail.com>
2025-12-02 10:01:48 +0530

    intial commit

---
+++ eclipse-workspace/com.aman/src/Array1D/Demo.java
@@ -0,0 +1,17 @@
+package Array1D;
+
+public class Demo {
+	public static void main(String[] args) {
+		
+		int arr[] = {12, 32, 321,23};
+		int sum=0;
+		
+		for(int i = 0; i<=arr.length; i++) {
+			sum=sum+arr[i];
+			
+			System.out.println(sum);
+		}
+		
+	}
+
+}
